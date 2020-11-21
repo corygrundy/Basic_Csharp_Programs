b76bdb61e9a77a7a44349c5b98069cc91c24323a
Virologic1717 <60462387+Virologic1717@users.noreply.github.com>
2020-11-21 12:42:17 -0800

    Update to file
    Adding new files and folders for further use.

---
+++ CsharpAssignment1/CsharpAssignment1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CsharpAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"acounting\" on you!");
+            Console.Read();
+        }
+    }
+}
---
+++ SampleApplication.cs
@@ -0,0 +1,10 @@
+using System;
+
+class SampleApplivation
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World");
+    }
+
+}
