2783168fdd1452fa1751ea6b49932fab929256f6
Virologic1717 <60462387+Virologic1717@users.noreply.github.com>
2020-11-22 13:35:56 -0800

    Upload file for assignment
    Uploading new files for assignment on page 168.

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(true && true && true || false);
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+            //console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            //console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Insureance Approval Program");
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int Age = Convert.ToInt32(age);
+            Console.WriteLine("Have you ever had a DUI");
+            string dui = Console.ReadLine();
+            bool DUI = Convert.ToBoolean(dui);
+            
+            Console.WriteLine("How many speeding tickets do you have?");
+            string speed = Console.ReadLine();
+            int Speed = Convert.ToInt32(speed);
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(Age > 15 && DUI == false && Speed <= 3);
+            Console.ReadLine();
+        }
+    }
+}
