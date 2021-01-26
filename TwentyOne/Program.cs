78ec1cbd207996ecd6ef84c2b470a3cc256c4f9d
Virologic1717 <60462387+Virologic1717@users.noreply.github.com>
2021-01-26 15:02:21 -0800

    Upload new file
    Uploading files for help from an instructor.

---
+++ TwentyOne/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + deck.Cards[0].Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
+        }
+    }
+}
