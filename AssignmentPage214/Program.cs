a3a4e52f47512499e88811e6a899b676b0075247
Virologic1717 <60462387+Virologic1717@users.noreply.github.com>
2021-01-26 14:24:37 -0800

    Upload new file
    Uploading new files for the assignment on page 214.

---
+++ AssignmentPage214/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPage214
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = new List<int> { 4, 26, 38, 68 };
+                Console.WriteLine("Enter a number to divide some integers.");
+                int ui1 = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                foreach (int item in list1)
+                {
+                        int answer1 = item / ui1;
+                        Console.WriteLine(answer1);
+                }
+            }
+            catch (FormatException ex)  //used to catch errors with different format together i.e. an integer "8" and a string "Cory".
+            {
+                Console.WriteLine("Please type a whole number.");
+                //return;    //This is used to return back to another page after an exception
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.WriteLine("The program has left the try/catch block.");
+            Console.ReadLine();
+        }
+    }
+}
--- ExceptionHandling/ExceptionHandling/Program.cs
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -6,14 +6,34 @@ namespace ExceptionHandling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pick a number.");
-            int numberOne = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Pick a second number.");
-            int numberTwo = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("dividing the two...");
-            int numberThree = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(numberOne + " divided by" + numberTwo + " equals " + numberThree);
-            Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Pick a number.");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number.");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("dividing the two...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+                Console.ReadLine();
+            }
+            catch (FormatException ex)  //used to catch errors with different format together i.e. an integer 8 and a string Cory.
+            {
+                Console.WriteLine("Please type a whole number.");
+                //return;    //This is used to return back to another page after an exception
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
