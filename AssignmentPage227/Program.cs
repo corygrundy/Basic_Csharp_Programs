00120c19d8f035ea79c7989bb8b67874c8a2f0a2
Virologic1717 <60462387+Virologic1717@users.noreply.github.com>
2021-01-28 10:31:44 -0800

    Update Program.cs
    Update to AssignmentPage227 for instructor approval.

--- AssignmentPage227/Program.cs
+++ AssignmentPage227/Program.cs
@@ -13,9 +13,18 @@ namespace AssignmentPage227
             Console.WriteLine("Please input a number.");
             int ui1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("You may add a second number here, otherwise leave it blank.");
-            int ui2 = Convert.ToInt32(Console.ReadLine());
-            int result1 = Class1.MethodAdd1(ui1, ui2);
-            Console.WriteLine("The result is " + result1);
+            string input2 = Console.ReadLine();
+            if (input2 == "")
+            {
+                int result1 = Class1.MethodAdd1(ui1);
+                Console.WriteLine("The result is " + result1);
+            }
+            else
+            {
+                int ui2 = Convert.ToInt32(input2);
+                int result1 = Class1.MethodAdd1(ui1, ui2);
+                Console.WriteLine("The result is " + result1);
+            }
             Console.ReadLine();
         }
     }
