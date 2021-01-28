b6ddedc5423da0395c0155867fba5f58da2823fa
Virologic1717 <60462387+Virologic1717@users.noreply.github.com>
2021-01-27 16:58:07 -0800

    Upload files
    Uploading new files for verification.

---
+++ AssignmentPage227/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentPage227
+{
+    class Class1
+    {
+        public static int MethodAdd1(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+    }
+}
---
+++ AssignmentPage227/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPage227
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number.");
+            int ui1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You may add a second number here, otherwise leave it blank.");
+            int ui2 = Convert.ToInt32(Console.ReadLine());
+            int result1 = Class1.MethodAdd1(ui1, ui2);
+            Console.WriteLine("The result is " + result1);
+            Console.ReadLine();
+        }
+    }
+}
