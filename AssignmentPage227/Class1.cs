133e1e87d1f3df23218c5df3ae6a58f66aeb3e3b
Virologic1717 <60462387+Virologic1717@users.noreply.github.com>
2021-01-27 17:00:07 -0800

    Update files
    Updating files from a last save.

--- AssignmentPage227/Class1.cs
+++ AssignmentPage227/Class1.cs
@@ -6,7 +6,7 @@ namespace AssignmentPage227
 {
     class Class1
     {
-        public static int MethodAdd1(int num1, int num2)
+        public static int MethodAdd1(int num1, int num2 = 3)
         {
             int result = num1 + num2;
             return result;
