3fa36b3f1019d626a29ead8560e1481492db88f1
Virologic1717 <60462387+Virologic1717@users.noreply.github.com>
2021-01-28 15:15:30 -0800

    Update files
    Update to files before work on next assignment.

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -10,6 +10,11 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string>() { "Jesse", "Bill", "Bob" };
+            game.ListPlayers();
+            Console.ReadLine();
+
             Deck deck = new Deck();
             deck.Shuffle(3);
 
--- TwentyOne/TwentyOneGame.cs
+++ TwentyOne/TwentyOneGame.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace TwentyOne
 {
     public class TwentyOneGame : Game
     {
-        public void Play()
+        public override void Play()
         {
             throw new NotImplementedException();
+        }
+        public override void ListPlayers()
+        {
+            Console.WriteLine("21 Players:");
+            base.ListPlayers();
         }
     }
 }
