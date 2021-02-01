cc34c7672dbbed73263defe2880b950cab9b26ce
Virologic1717 <60462387+Virologic1717@users.noreply.github.com>
2021-02-01 11:51:06 -0800

    Update Program.cs
    Update from following instructor in video.

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -10,15 +10,13 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            //deck.Shuffle(3);
 
-            Deck deck = new Deck();
-            deck.Shuffle(3);
-
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
         }
     }
