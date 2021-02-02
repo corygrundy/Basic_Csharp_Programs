ef4e6de177f7e65267814a998f45cd95740d7209
Virologic1717 <60462387+Virologic1717@users.noreply.github.com>
2021-02-01 21:35:55 -0800

    Update Program.cs
    Uploading changes made wayyyy too late...

--- AssignmentPage244/Program.cs
+++ AssignmentPage244/Program.cs
@@ -13,7 +13,7 @@ namespace AssignmentPage244
             Employee<string> Things = new List<string>();
             //Begin part 5     //Added <string> for part 3 on page 255
             Employee<string> emp1 = new Employee<string>();
-            emp1.Things = new List<string> { "Sample info.", "Sample", "Student", };
+            emp1.Things = new List<string> { "Sample info.", "Sample", "Student", "001" };
             emp1.firstName = "Sample";
             emp1.lastName = "Student";
             emp1.Id = 001;  //Also part of part 1 on page 252
@@ -23,7 +23,7 @@ namespace AssignmentPage244
 
             //Part of part 1 on page 252
             Employee<string> emp2 = new Employee<string>();
-            emp2.Things = new List<string> { "My Father." };
+            emp2.Things = new List<string> { "My Father.", "Jack", "Grundy", "002" };
             emp2.firstName = "Jack";
             emp2.lastName = "Grundy";
             emp2.Id = 002;
@@ -37,9 +37,45 @@ namespace AssignmentPage244
             emp3.firstName = "Cory";
             emp3.lastName = "Grundy";
             emp3.Id = 003;
-            
 
+            //Begin part 1
+            Employee<string> emp4 = new Employee<string>();
+            emp3.Things = new List<string> { "Caleb", "Grundy", "004" };
+            emp3.firstName = "Caleb";
+            emp3.lastName = "Grundy";
+            emp3.Id = 004;
+            Employee<string> emp5 = new Employee<string>();
+            emp3.Things = new List<string> { "Chase", "Sabre", "005" };
+            emp3.firstName = "Chase";
+            emp3.lastName = "Sabre";
+            emp3.Id = 005;
+            Employee<string> emp6 = new Employee<string>();
+            emp3.Things = new List<string> { "Joe", "Laphardy", "006" };
+            emp3.firstName = "Joe";
+            emp3.lastName = "Laphardy";
+            emp3.Id = 006;
+            Employee<string> emp7 = new Employee<string>();
+            emp3.Things = new List<string> { "Valdus", "White", "007" };
+            emp3.firstName = "Valdus";
+            emp3.lastName = "White";
+            emp3.Id = 007;
+            Employee<string> emp8 = new Employee<string>();
+            emp3.Things = new List<string> { "Logan", "Vegas", "008" };
+            emp3.firstName = "Logan";
+            emp3.lastName = "Vegas";
+            emp3.Id = 008;
+            Employee<string> emp9 = new Employee<string>();
+            emp3.Things = new List<string> { "Joe", "Johnson", "009" };
+            emp3.firstName = "Joe";
+            emp3.lastName = "Johnson";
+            emp3.Id = 009;
+            Employee<string> emp10 = new Employee<string>();
+            emp3.Things = new List<string> { "Patrick", "Roy", "010" };
+            emp3.firstName = "Patrick";
+            emp3.lastName = "Roy";
+            emp3.Id = 010;
 
+            //Last part of page 255
             foreach (var element in emp1.Things)
             {
                 Console.WriteLine(element);
