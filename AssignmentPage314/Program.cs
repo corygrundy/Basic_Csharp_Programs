9f6843f01747e896075219ed2b7b265f2e04dc61
Virologic1717 <60462387+Virologic1717@users.noreply.github.com>
2021-02-07 11:18:35 -0800

    Update of file Program.cs and addition of new class
    Updated Program.cs to ensure errors worked properly and to ensure math was correct.

--- AssignmentPage314/Program.cs
+++ AssignmentPage314/Program.cs
@@ -19,22 +19,36 @@ namespace AssignmentPage314
                 {
                     Console.WriteLine("What is your age?");
                     validAnswer = int.TryParse(Console.ReadLine(), out userAge);
+                    if (userAge < 0 || userAge <= -1)
+                    {
+                        throw new NumberException();
+                    }
+                    else if (userAge == 0)
+                    {
+                        throw new ZeroException();
+                    }
+                    else
                     Console.WriteLine(currentYear - userAge);
                     Console.ReadLine();
                 }
-                if (userAge < 0 || userAge <= -1)
-                {
-                    throw new NumberException();
-                }
+                
+            }
+            catch (ZeroException)
+            {
+                Console.WriteLine("Error. User must not enter their age as 0.");
+                Console.ReadLine();
+                return;
             }
             catch (NumberException)
             {
-                Console.WriteLine("Error. User must enter a whole number.");
+                Console.WriteLine("Error. User must enter a positive whole number.");
+                Console.ReadLine();
                 return;
             }
             catch (Exception)
             {
                 Console.WriteLine("Uh oh, something went wrong.");
+                Console.ReadLine();
                 return;
             }
         }
---
+++ AssignmentPage314/ZeroException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentPage314
+{
+    class ZeroException : Exception
+    {
+        public ZeroException()
+            : base() { }
+        public ZeroException(string message)
+            : base(message) { }
+    }
+}
