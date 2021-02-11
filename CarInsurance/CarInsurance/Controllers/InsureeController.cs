1684e2cc3af4316e18008875c26fc223bfd8c57d
Virologic1717 <60462387+Virologic1717@users.noreply.github.com>
2021-02-10 20:48:57 -0800

    Update InsureeController.cs
    Updating for a bit more help from an instructor.

--- CarInsurance/CarInsurance/Controllers/InsureeController.cs
+++ CarInsurance/CarInsurance/Controllers/InsureeController.cs
@@ -41,6 +41,9 @@ namespace CarInsurance.Controllers
             return View();
         }
 
+        public decimal v = 100;
+        public decimal c = 25;
+        // public decimal z = 50;    /* Unneeded decimal that I am leaving here for personal note that I don't need to make new variables if I need to
         // POST: Insuree/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
@@ -50,6 +53,81 @@ namespace CarInsurance.Controllers
         {
             if (ModelState.IsValid)
             {
+                decimal quoteCalc;
+                var age = DateTime.Now.Year - insuree.DateOfBirth.Year;
+                // insuree.Quote = 50;
+                if ( age < 18 )
+                {
+                    decimal x = 50;
+                    quoteCalc = x + v;
+                }
+                else if (age < 19 || age >25)
+                {
+                    decimal x = 50;
+                    quoteCalc = x + x;
+                }
+                else
+                {
+                    decimal x = 50;
+                    quoteCalc = x + c;
+                }
+                int carYear = insuree.CarYear;
+                decimal yearPrice;
+                if (carYear < 2000 || carYear > 2015)
+                {
+                    yearPrice = 25;
+                }
+                else
+                {
+                    yearPrice = 0;
+                }
+                string carMake = insuree.CarMake;
+                decimal makePrice;
+                if (carMake == "Porsche" || carMake == "porsche")
+                {
+                    makePrice = 25;
+                }
+                else
+                {
+                    makePrice = 0;
+                }
+                string carModel = insuree.CarModel;
+                decimal modelPrice;
+                if (carModel == "911 Carrera" || carModel == "911 carrera")
+                {
+                    modelPrice = 25;
+                }
+                else
+                {
+                    modelPrice = 0;
+                }
+                int speedFine = insuree.SpeedingTickets;
+                decimal speedPrice;
+                if (speedFine > 1)
+                {
+                    speedPrice = speedFine * 10;
+                }
+                else
+                {
+                    speedPrice = 0;
+                }
+                decimal quoteSum;
+                decimal quoteSum1;
+                bool dui = insuree.DUI;
+                decimal perc1 = 25;
+                if (dui == true)
+                {
+                    quoteSum1 = 50 + quoteCalc + yearPrice + makePrice + modelPrice + speedPrice;
+                    quoteSum = quoteSum1 * perc1 / 100;
+                }
+                else
+                {
+                    quoteSum1 = 50 + quoteCalc + yearPrice + makePrice + modelPrice + speedPrice;
+                    quoteSum = quoteSum1;
+                }
+                //bool fullCoverage = insuree.CoverageType
+                //if ()
+                insuree.Quote = quoteSum;
                 db.Insurees.Add(insuree);
                 db.SaveChanges();
                 return RedirectToAction("Index");
